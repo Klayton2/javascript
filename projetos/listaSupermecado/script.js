28ae120dbe5cf837debbf008e80b68175fb994f5
Klayton Rodrigues De Sousa <147443157+Klayton2@users.noreply.github.com>
2024-09-26 11:49:17 -0300

    26/09
    Fiz um projeto para criar e recuperar uma lista de mercado

---
+++ projetos/listaSupermecado/script.js
@@ -0,0 +1,50 @@
+var itens = [];
+
+document.querySelector('input[type=submit]').addEventListener('click',()=>{
+    var nomeProduto = document.querySelector('input[name=nome_produto]');
+
+    var valorProduto = document.querySelector('input[name=valor_produto');
+    itens.push({
+        nome: nomeProduto.value,
+        valor: valorProduto.value
+    });
+    let listaProduto = document.querySelector('.lista-produto');
+
+    let soma = 0;
+
+    listaProduto.innerHTML = "";
+    
+    itens.map((val)=>{
+        soma+=parseFloat(val.valor);
+
+        listaProduto.innerHTML+=
+            `<div class="lista-produto-single">
+                <h3>`+ val.nome+`</h3>
+                <h3 class="price-produto"><span>R$ `+val.valor+`</span></h3>
+            </div>
+            `;
+    })
+    let total = document.querySelector('.soma-produto h1');
+
+    total.innerHTML= `Total: R$ `+soma;
+    
+    soma = soma.toFixed(2);
+
+    nomeProduto.value ="";
+
+    valorProduto.value ="";
+    
+});
+
+document.querySelector('button[name=limpar]').addEventListener('click',()=>{
+    itens = [];
+    
+    document.querySelector('.lista-produto').innerHTML = "";
+
+    document.querySelector('.soma-produto h1').innerHTML = "Total: R$ 0";
+})
+
+/*<div class="lista-produto-single">
+                <h3>`+ val.nome+`</h3>
+                <h3 class="price-produto"><span>R$ `+val.valor+`</span></h3>
+        </div> */
