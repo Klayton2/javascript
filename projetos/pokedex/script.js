ae413ac7239918fd9b612666428aad9b989d52a8
Klayton Rodrigues De Sousa <147443157+Klayton2@users.noreply.github.com>
2024-11-01 16:35:11 -0300

    pokedex
    finalizei a pokedex

--- projetos/pokedex/script.js
+++ projetos/pokedex/script.js
@@ -1,3 +1,49 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-.then(response => response.json())
-.then(allpokemon => console.log(allpokemon))
+function pegaPokemon (quantidade) {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade)
+    .then(response => response.json())
+    .then(allpokemon => {
+    pokemons = [];
+
+    allpokemon.results.map((val) => {
+        fetch(val.url)
+        .then(response => response.json())
+        .then(pokemonSingle => {
+            pokemons.push({nome:val.name,imagem:pokemonSingle.sprites.front_default});
+
+            if(pokemons.length == quantidade) {
+
+                var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                pokemonBoxes.innerHTML = "";
+
+
+                pokemons.map((val) => {
+                    pokemonBoxes.innerHTML += 
+                    `<div class="pokemon-box">
+                        <img src="`+val.imagem+`">
+                        <p>`+val.nome+`</p>
+                    </div>
+                    `
+                })
+            }
+        })
+    })
+})
+}
+
+document.getElementById('enviar').addEventListener('click',() => {
+    let qntd = document.getElementById('quantidade').value;
+
+    if(qntd > 0){
+        reset();
+        pegaPokemon(qntd);
+    }else{
+        alert('Escolha um número maior do que zero !!');
+    }
+})
+
+function reset(){
+    document.getElementById('reset').addEventListener('click', () => {
+        let pokemon = document.querySelector('.pokemon-boxes');
+        pokemon.innerHTML = "";
+    })
+}
