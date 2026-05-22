d8052a0239afdded8af4ea0e3080bbd359e27461
Klayton Rodrigues De Sousa <147443157+Klayton2@users.noreply.github.com>
2026-05-22 13:27:03 -0300

    22/05
    me aprofundei no conceito de condiçoes

---
+++ modulo002/estudojs/condicoes.js
@@ -0,0 +1,7 @@
+var vel = 60;
+
+if(vel > 60){
+    console.log(`Você está acima da velocidade permitida. MULTADO !`);
+}else{
+    console.log(`Você está a ${vel} km/h, dirija com responsabilidade`);
+}
